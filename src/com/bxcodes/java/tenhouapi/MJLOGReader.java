e48cbd9b94df5ff7e37474b0e119dde2ecc7ac98
boxuan <boxuana@163.com>
2018-05-30 22:52:17 -0400

    05/30/2018

---
+++ src/com/bxcodes/java/tenhouapi/MJLOGReader.java
@@ -0,0 +1,17 @@
+package com.bxcodes.java.tenhouapi;
+
+import java.io.File;
+
+public class MJLOGReader {
+	public MJLOGReader() {
+		
+	}
+	
+	public void load(File f) {
+		
+	}
+	
+	
+	
+	private File f;
+}
